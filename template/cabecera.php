ad44cea8fe4f37559f3ad35b26a59f10fa93557b
Don-Victor-123 <109880765+Don-Victor-123@users.noreply.github.com>
2024-03-30 23:48:33 -0600

    Add files via upload

---
+++ template/cabecera.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <title>Sitio web</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php"> VictorDV </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"> Inicio </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="cursos.php"> Cursos </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="nosotros.php"> Nosotros </a>
+            </li>
+        </ul>
+    </nav>
+
+<div class="container">
+<br/>
+    <div class="row">
